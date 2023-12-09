3b3f6771717573041fd5536de34eaee42e3ce7f8
sethu <sethuvenkat003@gmail.com>
2023-12-09 23:05:28 +0530

    fusiontechbecapable3

---
+++ app/src/main/java/com/example/fusiontechbcapble/objectives.java
@@ -0,0 +1,14 @@
+package com.example.fusiontechbcapble;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class objectives extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_objectives);
+    }
+}
